63ed74dd2e0aeea5a6c48e1d1bedcf3fb1d7072c
User <savic.bu17@gmail.com>
2025-11-08 11:04:59 +0200

    init

--- mine.py
+++ mine.py
@@ -1,1 +1,21 @@
-print("Hello world")
+class Marker:
+    def __init__(self, color, size):
+        self.marker_color = color
+        self.marker_size = size
+
+    def draw(self):
+        print(f"Малюю кольором: {self.marker_color}, товщина лінії: {self.marker_size}")
+
+    def info(self):
+        print(f"Маркер {self.marker_color}, товщина {self.marker_size}")
+
+# Створення об’єктів
+m_red = Marker("червоний", 2)
+m_blue = Marker("синій", 4)
+
+# Виклик методів
+m_red.draw()
+m_blue.draw()
+
+m_red.info()
+m_blue.info()
